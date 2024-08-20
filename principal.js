c77d67f6966103515399fe4f5a6cf9ed3f80434f
Pedro Petterini <ppetterini@gmail.com>
2024-08-20 20:33:34 -0300

    commit

--- principal.js
+++ principal.js
@@ -5,4 +5,4 @@ function trocaBanner(){
 	banner = (banner + 1) % 2;
 	document.querySelector("h2#mensagem").textContent = banners[banner];
 }
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
