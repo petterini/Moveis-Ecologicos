e29a4b9a4d90f80fb33fd7e20aa70c4e4a5a38a6
Pedro Petterini <ppetterini@gmail.com>
2024-08-20 20:10:38 -0300

    Agora sim

---
+++ principal.js
@@ -0,0 +1,8 @@
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo"];
+var banner = 0;
+
+function trocaBanner(){
+	banner = (banner + 1) % 2;
+	document.querySelector("h2#mensagem").textContent = banners[banner];
+}
+setInterval(trocaBanner, 2000);
